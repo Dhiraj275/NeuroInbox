11bd88dbf1f32b9b58149ba4a14a602a3930942c
Dhiraj275 <dhirajprajapati.co@gmail.com>
2026-08-22 22:50:03 +0530

    fix: insert sms into system db while sending

--- android/app/src/main/java/com/dhiraj/neuroinbox/DefaultSmsModule.kt
+++ android/app/src/main/java/com/dhiraj/neuroinbox/DefaultSmsModule.kt
@@ -145,6 +145,20 @@ class DefaultSmsModule(reactContext: ReactApplicationContext) :
                 smsManager.sendTextMessage(phoneNumber, null, message, null, null)
             }
 
+            // Store sent message in Android System Telephony Provider
+            try {
+                val values = ContentValues().apply {
+                    put(Telephony.Sms.ADDRESS, phoneNumber)
+                    put(Telephony.Sms.BODY, message)
+                    put(Telephony.Sms.DATE, System.currentTimeMillis())
+                    put(Telephony.Sms.READ, 1)
+                    put(Telephony.Sms.TYPE, Telephony.Sms.MESSAGE_TYPE_SENT)
+                }
+                reactApplicationContext.contentResolver.insert(Telephony.Sms.Sent.CONTENT_URI, values)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
             promise.resolve(true)
         } catch (e: Exception) {
             promise.reject("E_SEND_SMS_FAILED", e.message, e)
